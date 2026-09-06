94dae5efc2d487b3bf820c1459589c4e79f88ce3
Eric Park <79054343+cire21st@users.noreply.github.com>
2026-09-06 14:11:55 +0900

    Add reference URL to 두 수의 곱구하기.py
    Added a URL comment for reference.

--- Lv0/두 수의 곱구하기.py
+++ Lv0/두 수의 곱구하기.py
@@ -1,3 +1,4 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120804
 def solution (num1: int, num2: int) -> int:
     if (0 <= num1 <= 100) and (0 <= num2 <= 100):
         return num1 * num2
